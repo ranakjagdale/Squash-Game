8f8bcbe1165cb072e4b6f4939d99b621a12992f8
ranakjagdale <65115164+ranakjagdale@users.noreply.github.com>
2020-05-14 21:08:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+var ballimg,paddleimg;
+var ball,paddle;
+function preload() {
+  /* preload your images here of the ball and the paddle */
+  ballimg = loadImage('ball.png');
+  paddleimg = loadImage('paddle.png');  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  
+  /* create the Ball Sprite and the Paddle Sprite */
+  ball = createSprite(200,200,12,12);
+  paddle = createSprite(380,200,12,70);
+   
+  /* assign the images to the sprites */
+  ball.addImage("ball",ballimg);
+  paddle.addImage("paddle",paddleimg);
+  /* give the ball an initial velocity of 9 in the X direction */
+  
+   ball.velocityX = 9;
+}
+
+function draw() {
+  background(205,153,0);
+  
+  /* create Edge Sprites here */
+  edges = createEdgeSprites();
+  
+  
+    /* Allow the ball sprite to bounceOff the left, top and bottom edges only, leaving the right edge of the canvas to be open. */
+  if (ball.isTouching(edges[0]) || ball.isTouching(edges[2]) || ball.isTouching(edges[3])) {
+    ball.bounceOff(edges[0]);
+    ball.bounceOff(edges[2]);
+    ball.bounceOff(edges[3]);
+  }
+  
+  /* Allow the ball to bounceoff from the paddle */
+  ball.bounceOff(paddle);
+  
+    /* Also assign a collision callback function, so that the ball can have a random y velocity, making the game interesting */
+ 
+  
+  ball.bounceOff(paddle, (randomVelocity));
+     
+ 
+
+  /* Prevent the paddle from going out of the edges */ 
+  
+  paddle.bounceOff(edges);
+  
+  if(keyIsDown(UP_ARROW))
+  {
+     /* what should happen when you press the UP Arrow Key */
+    paddle.velocityY = -5;
+  }
+  
+  if (keyIsDown(DOWN_ARROW))  
+  {
+    /* what should happen when you press the UP Arrow Key */    
+    paddle.velocityY = 5;
+  }
+  drawSprites();
+  
+}
+
+function randomVelocity()
+{
+  /* this function gets called when the ball bounces off the paddle */
+  /* assign the ball a random vertical velocity, so it bounces off in random direction */
+  return  random(-5,5);
+   //ball.velocityX = random(-1,-9);       
+   
+}
